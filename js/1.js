73cddb76b7b6b5709f18a4ed8122a90c7c0912fd
panyilin <406612515@qq.com>
2020-07-14 16:18:58 +0800

    frist send

---
+++ js/1.js
@@ -0,0 +1,10 @@
+const http=require('http');
+const fs=require('fs');
+const path=require('path');
+const server=http.createServer();
+server.on('request',function(req,res){
+    
+});
+server.listen('8099',function(){
+    
+})
